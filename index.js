63495ed1eaa52e8110857774b99d41a04fce252d
jpfraneto <jpfraneto@gmail.com>
2023-05-05 14:39:35 -0400

    updated the thing0

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const whatsappClient = require('./whatsappClient');
 
+whatsappClient.on('ready', () => {
+  console.log('WhatsApp client is ready!');
+
+  app.use(limiter);
+  app.use(helmet());
+
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: 'An error occurred. Please try again later.' });
+  });
+
+  app.get('/api/hello', (req, res) => {
+    res.send({ message: 'Hello World' });
+  });
+
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening at http://0.0.0.0:${port}`);
+  });
+});
 
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
-app.use(limiter);
-app.use(helmet());
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({ error: 'An error occurred. Please try again later.' });
-});
-
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello World' });
-});
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server listening at http://0.0.0.0:${port}`);
-});
-
+whatsappClient.initialize();
---
+++ prev.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const whatsappClient = require('./whatsappClient');
+
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+
+app.use(limiter);
+app.use(helmet());
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: 'An error occurred. Please try again later.' });
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello World' });
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening at http://0.0.0.0:${port}`);
+});
+
