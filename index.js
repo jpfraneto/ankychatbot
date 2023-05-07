ccb3a13160249d45491edb72dad1bc5abcefad90
jpfraneto <jpfraneto@gmail.com>
2023-05-06 20:31:52 -0400

    added the message to jp

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cron = require('node-cron');
 const qrcode = require('qrcode-terminal');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const prisma = require('./prismaClient');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
+const phoneNumber = '56985491126';
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
 app.use(limiter);
@@ -16,8 +20,19 @@ app.use((err, req, res, next) => {
   res.json({ error: 'An error occurred. Please try again later.' });
 });
 
+app.get('/', (req, res) => {
+  res.send({ message: 'wena compare!!' });
+});
+
 app.get('/api/hello', (req, res) => {
   res.send({ message: 'Hello World' });
+});
+
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany({
+    where: {},
+  });
+  res.json(users);
 });
 
 const client = new Client({
@@ -31,6 +46,16 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
   console.log('WhatsApp client is ready!');
+
+  cron.schedule('0 * * * *', async () => {
+    const timestamp = new Date().toLocaleString();
+    const messageText = `Hello ${timestamp}`;
+
+    const id = { id: { _serialized: `whatsapp:${phoneNumber}@c.us` } };
+    const info = await client.sendMessage(id, messageText);
+
+    console.log(`Message sent: ${messageText}`);
+  });
 });
 
 client.on('message', async message => {
